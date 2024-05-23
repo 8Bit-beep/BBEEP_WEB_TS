4d3229b6cb43b6ea753b3b8bc85c0251b4d406c5
jeonminchan <132974029+wjzlskxk@users.noreply.github.com>
2024-05-23 21:24:26 +0900

    feat :: HEADER, SIDEBAR #2

---
+++ src/components/router/router.tsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "src/components/auth/Login";
+import NoneLoadingMain from "src/components/home/index";
+import CheckStudent from "src/components/home/student/studentOne";
+import CheckStudentSecond from "src/components/home/student/studentTow";
+import CheckStudentThird from "src/components/home/student/studentThree";
+import CheckClass from "src/components/home/class/ClassOne";
+import CheckClassSecond from "src/components/home/class/classTwo";
+import CheckClassThird from "src/components/home/class/ClassThree";
+const router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/main" element={<NoneLoadingMain />} />
+      <Route path="/check-student/first-grade" element={<CheckStudent />} />
+      <Route path="/check-student/second-grade" element={<CheckStudentSecond />} />
+      <Route path="/check-student/third-grade" element={<CheckStudentThird />} />
+      <Route path="/check-class/first-floor" element={<CheckClass />} />
+      <Route path="/check-class/second-floor" element={<CheckClassSecond />} />
+      <Route path="/check-class/third-floor" element={<CheckClassThird />} />
+    </Routes>
+  );
+};
+
+export default router;
--- src/hooks/common/header/useHeader.ts
+++ src/hooks/common/header/useHeader.ts
@@ -1,5 +1,77 @@
-const useHeader = () => {
+import { bbeepAxios } from "src/libs/axios/customAxios";
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { NavigationProps, TeacherInfo } from "src/types/common/header/header.types";
 
-}
+const useHeader = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [isClicked, setIsClicked] = useState<string>("학생 조회하기");
+  const [info, setInfo] = useState<TeacherInfo>({
+    name: "",
+    email: "",
+    department: "",
+    job: "",
+  });
+
+  const selectTeacherInfo = async () => {
+    bbeepAxios.get("/teachers/info").then((res) => {
+      setInfo(res.data);
+      console.log(info.job);
+    });
+  };
+
+  const HandleNavigation = ({ location, navigate }: NavigationProps) => {
+    switch (location.pathname) {
+      case "/check-student/first-grade":
+        setIsClicked("학생 조회하기");
+        break;
+      case "/check-student/second-grade":
+        setIsClicked("학생 조회하기");
+        break;
+      case "/check-student/third-grade":
+        setIsClicked("학생 조회하기");
+        break;
+      case "/check-class/first-floor":
+        setIsClicked("실 조회하기");
+        break;
+      case "/check-class/second-floor":
+        setIsClicked("실 조회하기");
+        break;
+      case "/check-class/third-floor":
+        setIsClicked("실 조회하기");
+        break;
+      default:
+        setIsClicked("");
+        break;
+    }
+  };
+
+  const HandleCategoryClick = (itemName: string) => {
+    setIsClicked(itemName);
+    switch (itemName) {
+      case "학생 조회하기":
+        navigate("/check-student/first-grade");
+        break;
+      case "실 조회하기":
+        navigate("/check-class/first-floor");
+        break;
+      default:
+        navigate("");
+    }
+  };
+
+  useEffect(() => {
+    HandleNavigation({ location, navigate });
+  }, [location.pathname]);
+
+  return {
+    info,
+    isClicked,
+    HandleNavigation,
+    HandleCategoryClick,
+    selectTeacherInfo,
+  };
+};
 
 export default useHeader;
